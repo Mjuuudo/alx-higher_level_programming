63b65af25d7ddd2360f7467ccb1b6cf1c4078a49
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-10-31 14:45:31 -0700

    Correct Answer

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for letter in range (97, 123):
+    if (letter == 113):
+        continue
+    if (letter == 101):
+        continue
+    print("{}".format(chr(letter)), end="")
