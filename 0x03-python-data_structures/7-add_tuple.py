530303583704fa3c01c0ea2a863db908917a7606
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-11-07 02:50:36 -0800

    Correct Answer

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-     if len(tuple_a) < 2:
+    """Add two tuples."""
+    if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
         else:
