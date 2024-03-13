82b209fca350163adee07a26fef39ee6b7d0e069
abait-ou <abdelmajid.aitouaziz@gmail.com>
2024-03-13 16:28:05 +0000

    Correct answer

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,52 @@
 #!/usr/bin/python3
-from calculator_1 import add,sub,mul,div
-import sys
+# from calculator_1 import add,sub,mul,div
+# import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif  len(sys.argv) == 4:
-        if sys.argv[2] == '+':
-            res = add(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == '-':
-            res = sub(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == '*':
-            res = mul(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == '/':
-            res = div(int(sys.argv[1]), int(sys.argv[3]))
-        else :
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    print(f"{int(sys.argv[1]):d}", end=" ")
-    print(f"{sys.argv[2]}", end=" ")
-    print(f"{int(sys.argv[3]):d}", end=" ")
-    print("=", end=" ")
-    print(f"{res:d}", end=" ")
-elif __name__ != "__main__":
+# if __name__ == "__main__":
+#     if len(sys.argv) != 4:
+#         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+#         exit(1)
+#     elif  len(sys.argv) == 4:
+#         if sys.argv[2] == '+':
+#             res = add(int(sys.argv[1]), int(sys.argv[3]))
+#         elif sys.argv[2] == '-':
+#             res = sub(int(sys.argv[1]), int(sys.argv[3]))
+#         elif sys.argv[2] == '*':
+#             res = mul(int(sys.argv[1]), int(sys.argv[3]))
+#         elif sys.argv[2] == '/':
+#             res = div(int(sys.argv[1]), int(sys.argv[3]))
+#         else :
+#             print("Unknown operator. Available operators: +, -, * and /")
+#             exit(1)
+#     print(f"{int(sys.argv[1]):d}", end=" ")
+#     print(f"{sys.argv[2]}", end=" ")
+#     print(f"{int(sys.argv[3]):d}", end=" ")
+#     print("=", end=" ")
+#     print(f"{res:d}", end=" ")
+# elif __name__ != "__main__":
+#     exit()
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
     exit()
+
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    func = add
+elif argv[2] == '-':
+    func = sub
+elif argv[2] == '*':
+    func = mul
+elif argv[2] == '/':
+    func = div
+else:
+    print("Unknown operator. Available operators: +, -, *, and /")
+    exit(1)
+
+result = func(int(argv[1]), int(argv[3]))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    argv[2], int(argv[3]), result))
