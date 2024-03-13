f69c6f40673aa18ccb6350aae0256b2e1354feb7
abait-ou <abdelmajid.aitouaziz@gmail.com>
2024-03-13 16:09:27 +0000

    test

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add,sub,mul,div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1);
+    elif  len(sys.argv) == 4:
+        if sys.argv[2] == '+':
+            res = add(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == '-':
+            res = sub(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == '*':
+            res = mul(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == '/':
+            res = div(int(sys.argv[1]), int(sys.argv[3]))
+        else :
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    print(f"{int(sys.argv[1]):d}", end=" ")
+    print(f"{sys.argv[2]}", end=" ")
+    print(f"{int(sys.argv[3]):d}", end=" ")
+    print("=", end=" ")
+    print(f"{res:d}", end=" ")
