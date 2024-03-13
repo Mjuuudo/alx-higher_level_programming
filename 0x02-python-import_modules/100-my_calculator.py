4f830a22cf3d7ce45b6f5e1704af812960b3158d
abait-ou <abdelmajid.aitouaziz@gmail.com>
2024-03-13 16:26:43 +0000

    test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 from calculator_1 import add,sub,mul,div
 import sys
 
-if __name__ != "__main__":
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -23,3 +23,5 @@ if __name__ != "__main__":
     print(f"{int(sys.argv[3]):d}", end=" ")
     print("=", end=" ")
     print(f"{res:d}", end=" ")
+elif __name__ != "__main__":
+    exit()
