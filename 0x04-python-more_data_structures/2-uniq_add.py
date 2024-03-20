b020dd68fd1d9b54b40738d64cbcf0fb8363be9f
abait-ou <abdelmajid.aitouaziz@gmail.com>
2024-03-20 11:14:41 +0000

    Correct Answer

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ def uniq_add(my_list=[]):
     new_list = []
     new_list = set(my_list)
     result = 0
-    for number in new_list :
+    for number in new_list:
         result += number
     return result
